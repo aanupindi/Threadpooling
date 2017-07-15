8087c761d87e10d93cce372b1ccec69177f3482f
Akhilesh Anupindi <ak.anupindi@gmail.com>
2017-07-15 00:45:41 -0700

    Fixed crashers on threadpool.cpp. Replaced storing raw thread objects in the vector with unique pointers.

--- threadpool.cpp
+++ threadpool.cpp
@@ -13,27 +13,29 @@ using namespace std::placeholders;
 class ThreadPool{
 	int numThreads;
 	std::queue<std::function<void()>> workerQueue;
-	std::vector<std::thread> workerThreads;
+	std::vector<std::unique_ptr<std::thread>> workerThreads;
 	std::mutex workerQMutex;
 	std::condition_variable workerQueueCV;
 	std::atomic_bool terminate;
 
 
 	void GetTask(){
+					std::cout << "Waiting...." << std::endl;
 		while(1){
 
 			//lock on the queue
+
 			std::unique_lock<std::mutex> tasklocker(workerQMutex);
 			std::cout << "Waiting for Task : Thread Id" << std::this_thread::get_id() << std::endl;
 			//Now check if there are any tasks in the Queue
 			workerQueueCV.wait(tasklocker, [&](){return !workerQueue.empty() || terminate.load() == true;});
-
+			std::cout<<"Working on task "<<std::endl;
 			//Exit condition
-			if(terminate.load() == false) 
-				{
-					std::cout << "terminate called. Exiting!" << std::endl;
-					return;
-				}
+			if(terminate.load() == true) 
+			{
+				std::cout << "terminate called. Exiting!" << std::endl;
+				return;
+			}
 			//comes here only if queue is not empty
 			std::function<void()> currentJob = workerQueue.front();
 			workerQueue.pop();
@@ -45,11 +47,6 @@ class ThreadPool{
 
 
 	}
-	void WaitForAllTasks(){
-	for(auto& threads : workerThreads){
-		threads.join();
-	}	
-}	
 
 public:
 	ThreadPool(){
@@ -59,21 +56,20 @@ public:
 	}
 
 	void Init(){
-		std::cout <<"CAlling init on all threads: "<<numThreads<<std::endl;
+		std::cout <<"Calling init on all threads: "<<numThreads<<std::endl;
 		for(int i = 0;i<numThreads; ++i){
-			workerThreads.emplace_back(std::thread(&ThreadPool::GetTask,this));
+			workerThreads[i] = std::unique_ptr<std::thread>(new std::thread(&ThreadPool::GetTask,this));
 		}
 	}
 
 	~ThreadPool(){
-
-		terminate.store(true);
-		workerQueueCV.notify_all();
-		WaitForAllTasks();
+		//WaitForAllTasks();
+		//workerThreads.clear();
 	}
 
 	void AddTask(std::function<void()> threadFunction){
 
+		std::cout <<"Adding task " << std::endl;
 		std::unique_lock<std::mutex> taskLocker(workerQMutex);
 		//function <void (int)> f = std::bind(threadFunction, x);
 		workerQueue.push(threadFunction);	 // bind returns a type of std::function<>
@@ -81,6 +77,17 @@ public:
 		taskLocker.unlock();
 		workerQueueCV.notify_one();	
 
+	}
+	void WaitForAllTasks(){
+		std::cout << "numThreads is " <<numThreads <<std::endl;
+		for(int i = 0;i<numThreads; ++i){
+			workerThreads[i]->join();
+		}
+	}	
+
+	void End(){		
+		terminate.store(true);
+		workerQueueCV.notify_all();
 	}
 		
 };
@@ -94,4 +101,8 @@ int main(int argc, char** argv){
 	ThreadPool pool1;
 	pool1.Init();
 	pool1.AddTask(Printfunction);
+	std::cout<<"before the end " <<std::endl;
+		pool1.End();
+	pool1.WaitForAllTasks();
+
 }
